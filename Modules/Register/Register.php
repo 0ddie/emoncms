d15d3897f6f8bfd096409e1a10ae8f59cc343a9d
Michael Oddie <meo9@aber.ac.uk>
2015-03-30 22:55:58 +0000

    Adding feed to register

--- Modules/Register/Register.php
+++ Modules/Register/Register.php
@@ -96,6 +96,63 @@ function RegiController()
                 return array('content' => $result);
                 return ("Ok");
             }
+            $feedid = (int)get('id');
+            // Actions that operate on a single existing feed that all use the feedid to select:
+            // First we load the meta data for the feed that we want
+
+            if ($feed->exist($feedid)) // if the feed exists
+            {
+                $f = $feed->get($feedid);
+                // if public or belongs to user
+                if ($f['public'] || ($session['userid'] > 0 && $f['userid'] == $session['userid'] && $session['read'])) {
+                    if ($route->action == "value") $result = $feed->get_value($feedid);
+                    if ($route->action == "timevalue") $result = $feed->get_timevalue_seconds($feedid);
+                    if ($route->action == "get") $result = $feed->get_field($feedid, get('field')); // '/[^\w\s-]/'
+                    if ($route->action == "aget") $result = $feed->get($feedid);
+
+                    if ($route->action == 'histogram') $result = $feed->histogram_get_power_vs_kwh($feedid, get('start'), get('end'));
+                    if ($route->action == 'kwhatpower') $result = $feed->histogram_get_kwhd_atpower($feedid, get('min'), get('max'));
+                    if ($route->action == 'kwhatpowers') $result = $feed->histogram_get_kwhd_atpowers($feedid, get('points'));
+                    if ($route->action == 'data') $result = $feed->get_data($feedid, get('start'), get('end'), get('dp'));
+                    if ($route->action == 'average') $result = $feed->get_average($feedid, get('start'), get('end'), get('interval'));
+                }
+
+                // write session required
+                //
+                /*if (isset($session['write']) && $session['write'] && $session['userid']>0 && $f['userid']==$session['userid'])
+                {
+                    // Storage engine agnostic
+                    if ($route->action == 'set') $result = $feed->set_feed_fields($feedid,get('fields'));
+                    if ($route->action == "insert") $result = $feed->insert_data($feedid,time(),get("time"),get("value"));
+                    if ($route->action == "update") $result = $feed->update_data($feedid,time(),get("time"),get('value'));
+                    if ($route->action == "delete") $result = $feed->delete($feedid);
+                    if ($route->action == "getmeta") $result = $feed->get_meta($feedid);
+
+                    if ($route->action == "csvexport") $feed->csv_export($feedid,get('start'),get('end'),get('interval'));
+
+                    if ($f['engine']==Engine::TIMESTORE) {
+                        if ($route->action == "export") $result = $feed->timestore_export($feedid,get('start'),get('layer'));
+                        if ($route->action == "exportmeta") $result = $feed->timestore_export_meta($feedid);
+                        if ($route->action == "scalerange") $result = $feed->timestore_scale_range($feedid,get('start'),get('end'),get('value'));
+                    } elseif ($f['engine']==Engine::MYSQL) {
+                        if ($route->action == "export") $result = $feed->mysqltimeseries_export($feedid,get('start'));
+                        if ($route->action == "deletedatapoint") $result = $feed->mysqltimeseries_delete_data_point($feedid,get('feedtime'));
+                        if ($route->action == "deletedatarange") $result = $feed->mysqltimeseries_delete_data_range($feedid,get('start'),get('end'));
+                    } elseif ($f['engine']==Engine::PHPTIMESERIES) {
+                        if ($route->action == "export") $result = $feed->phptimeseries_export($feedid,get('start'));
+                    } elseif ($f['engine']==Engine::PHPFIWA) {
+                        if ($route->action == "export") $result = $feed->phpfiwa_export($feedid,get('start'),get('layer'));
+                    } elseif ($f['engine']==Engine::PHPFINA) {
+                        if ($route->action == "export") $result = $feed->phpfina_export($feedid,get('start'));
+                    }
+                }
+            }
+            else
+            {
+                $result = array('success'=>false, 'message'=>'Feed does not exist');
+            }
+                */
+
             // So valid Json string to send should be: Register/create.json?data=123
             // }//while($timerout != $timerstart);
         }
@@ -187,6 +244,7 @@ function RegiController()
      *
      * };
      */
-    RegiController();
-}
+                RegiController();
+            }
+        }
 
